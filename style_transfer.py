4dafcbf5a56ec0df76f6451d788ecc5b64013755
jason9075 <jason9075@gmail.com>
2018-06-12 18:07:30 +0800

    git init

---
+++ style_transfer.py
@@ -0,0 +1,193 @@
+
+from keras.layers import Input, Lambda, Dense, Flatten
+from keras.layers import AveragePooling2D, MaxPooling2D
+from keras.layers.convolutional import Conv2D
+from keras.models import Model, Sequential
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing import image
+from skimage.transform import resize
+
+import keras.backend as K
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.misc
+
+from scipy.optimize import fmin_l_bfgs_b
+from datetime import datetime
+
+# 創建修改後的VGG 當核心 
+def VGG16_AvgPool(shape):
+  # we want to account for features across the entire image
+  # so get rid of the maxpool which throws away information
+  vgg = VGG16(input_shape=shape, weights='imagenet', include_top=False)
+
+  new_model = Sequential()
+  for layer in vgg.layers:
+    if layer.__class__ == MaxPooling2D:
+      # replace it with average pooling
+      new_model.add(AveragePooling2D())
+    else:
+      new_model.add(layer)
+
+  return new_model
+
+def VGG16_AvgPool_CutOff(shape, num_convs):
+  # there are 13 convolutions in total
+  # we can pick any of them as the "output"
+  # of our content model
+
+  if num_convs < 1 or num_convs > 13:
+    print("num_convs must be in the range [1, 13]")
+    return None
+
+  model = VGG16_AvgPool(shape)
+  new_model = Sequential()
+  n = 0
+  for layer in model.layers:
+    if layer.__class__ == Conv2D:
+      n += 1
+    new_model.add(layer)
+    if n >= num_convs:
+      break
+
+  return new_model
+
+def gram_matrix(img):
+  # input is (H, W, C) (C = # feature maps)
+  # we first need to convert it to (C, H*W)
+  X = K.batch_flatten(K.permute_dimensions(img, (2, 0, 1)))
+  
+  # now, calculate the gram matrix
+  # gram = XX^T / N
+  # the constant is not important since we'll be weighting these
+  G = K.dot(X, K.transpose(X)) / img.get_shape().num_elements()
+  return G
+
+
+def style_loss(y, t):
+  return K.mean(K.square(gram_matrix(y) - gram_matrix(t)))
+
+
+# let's generalize this and put it into a function
+def minimize(fn, epochs, batch_shape):
+  t0 = datetime.now()
+  losses = []
+  x = np.random.randn(np.prod(batch_shape))
+  for i in range(epochs):
+    x, l, _ = fmin_l_bfgs_b(
+      func=fn,
+      x0=x,
+      maxfun=20
+    )
+    x = np.clip(x, -127, 127)
+    print("iter=%s, loss=%s" % (i, l))
+    losses.append(l)
+
+  print("duration:", datetime.now() - t0)
+  # plt.plot(losses)
+  # plt.show()
+
+  newimg = x.reshape(*batch_shape)
+  final_img = unpreprocess(newimg)
+  return final_img[0]
+
+def load_img_and_preprocess(path, shape=None):
+  img = image.load_img(path, target_size=shape)
+
+  # convert image to array and preprocess for vgg
+  x = image.img_to_array(img)
+  x = np.expand_dims(x, axis=0)
+  x = preprocess_input(x)
+
+  return x
+
+def unpreprocess(img):
+  img[..., 0] += 103.939
+  img[..., 1] += 116.779
+  img[..., 2] += 126.68
+  img = img[..., ::-1]
+  return img
+
+
+content_img = load_img_and_preprocess(
+  'content/sydney.jpg',
+)
+
+# resize the style image
+# since we don't care too much about warping it
+h, w = content_img.shape[1:3]
+style_img = load_img_and_preprocess(
+  'styles/lesdemoisellesdavignon.jpg',
+  (h, w)
+)
+
+
+# we'll use this throughout the rest of the script
+batch_shape = content_img.shape
+shape = content_img.shape[1:]
+
+
+# we want to make only 1 VGG here
+# as you'll see later, the final model needs
+# to have a common input
+vgg = VGG16_AvgPool(shape)
+
+
+# create the content model
+# we only want 1 output
+# remember you can call vgg.summary() to see a list of layers
+# 1,2,4,5,7-9,11-13,15-17
+content_model = Model(vgg.input, vgg.layers[13].get_output_at(1))
+content_target = K.variable(content_model.predict(content_img))
+
+
+# create the style model
+# we want multiple outputs
+# we will take the same approach as in style_transfer2.py
+symbolic_conv_outputs = [
+  layer.get_output_at(1) for layer in vgg.layers \
+  if layer.name.endswith('conv1')
+]
+
+# make a big model that outputs multiple layers' outputs
+style_model = Model(vgg.input, symbolic_conv_outputs)
+
+# calculate the targets that are output at each layer
+style_layers_outputs = [K.variable(y) for y in style_model.predict(style_img)]
+
+# we will assume the weight of the content loss is 1
+# and only weight the style losses
+style_weights = [0.2,0.4,0.3,0.5,0.2]
+
+
+
+# create the total loss which is the sum of content + style loss
+loss = K.mean(K.square(content_model.output - content_target))
+
+for w, symbolic, actual in zip(style_weights, symbolic_conv_outputs, style_layers_outputs):
+  # gram_matrix() expects a (H, W, C) as input
+  loss += w * style_loss(symbolic[0], actual[0])
+
+
+# once again, create the gradients and loss + grads function
+# note: it doesn't matter which model's input you use
+# they are both pointing to the same keras Input layer in memory
+grads = K.gradients(loss, vgg.input)
+
+# just like theano.function
+get_loss_and_grads = K.function(
+  inputs=[vgg.input],
+  outputs=[loss] + grads
+)
+
+
+def get_loss_and_grads_wrapper(x_vec):
+  l, g = get_loss_and_grads([x_vec.reshape(*batch_shape)])
+  return l.astype(np.float64), g.flatten().astype(np.float64)
+
+
+final_img = minimize(get_loss_and_grads_wrapper, 1, batch_shape)
+# plt.imshow(scale_img(final_img))
+# plt.show()
+scipy.misc.imsave('results/result.jpg', final_img)
